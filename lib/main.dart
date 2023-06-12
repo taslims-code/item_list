6fb005c1f384acd07cea5536a9570ea8970719f5
taslims-code <taslimul39@gmail.com>
2023-06-12 23:24:50 +0600

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -21,10 +22,48 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  List<String> myList = ['Apples', 'Bananas', 'Bread', 'Milk', 'Eggs'];
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'My Shopping List',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Cart is empty')));
+                },
+                icon: Icon(
+                  Icons.shopping_cart,
+                  size: 26,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: myList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(4),
+                leading: Icon(
+                  Icons.shopping_basket,
+                  size: 26,
+                  color: Colors.grey,
+                ),
+                title: Text(myList[index]),
+              ),
+            );
+          },
+        ));
   }
 }
